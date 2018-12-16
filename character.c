e23dd4f9b3b98cf83fec487b8fab5f5ce1dd8af2
ut4k <aquarium011@gmail.com>
2018-12-16 19:41:24 +0900

    asciiコードを表示

---
+++ character.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	int one_alphabet;
+	int one_number;
+
+	one_alphabet = 65;
+	one_number = 48;
+	
+	printf("one_alphabet is %c\n", one_alphabet);
+	printf("one_number is %c\n", one_number);
+
+	return 0;
+}
