efa106f1212c8ba8f5eb394e1dd002edc0495656
ut4k <aquarium011@gmail.com>
2018-12-16 19:35:05 +0900

    初期値を入れない変数を表示

---
+++ no_init.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+	char character;
+	int number;
+
+	printf("character is = %c\n", character);
+	printf("number is = %d\n", number);
+	// Archで実行
+	//character is =
+	// number is = 0
+	return 0;
+}
