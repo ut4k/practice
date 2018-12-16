f4edd1809839d8fc93bf1ccb370cbac66d6b63ed
ut4k <aquarium011@gmail.com>
2018-12-16 21:32:19 +0900

    if...else

---
+++ if_else.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	int a = 1;
+
+	if (a == 1){
+		printf("a is 1\n");
+	}else if(a == 2){
+		printf("a is 2\n");
+	}else{
+		printf("a is neither 1 nor 2\n");
+	}
+	return 0;
+}
